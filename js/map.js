e0d8f487c4ece1e33209693a79f4b3a0f0a4ce69
JanVan01 <j_vanz01@uni-muenster.de>
2016-12-05 09:10:12 +0100

    add attribution + formatting

--- js/map.js
+++ js/map.js
@@ -1,16 +1,17 @@
- 
+var map = L.map('map', {
+    center: [51.959312, 7.617267],
+    zoom: 12,
+    minZoom: 11
+});
 
-var map = L.map('map',{
- center: [51.959312,7.617267],
- zoom: 12,
- minZoom: 11
- });
- 
- 
+map.setMaxBounds([
+    [51.8675880511, 7.3663330078],
+    [52.0554460954, 7.8524780273]
+]);
 
- map.setMaxBounds([[51.8675880511,7.3663330078], [52.0554460954,7.8524780273]]);
-var osmLayer = new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { noWrap: true}).addTo(map);
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    noWrap: true,
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-
-
- var sidebar = L.control.sidebar('sidebar').addTo(map);
+L.control.sidebar('sidebar').addTo(map);
