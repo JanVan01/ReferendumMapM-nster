41baaae03ecb3a9f3eb6b41e85d7727f98d15c20
s_khal01 <s_khal01@uni-muenster.de>
2016-12-05 03:00:38 +0100

    Lorem ipsum text removed and bounding box added

--- js/map.js
+++ js/map.js
@@ -1,11 +1,16 @@
- var map = L.map('map',{
- center: [51.959312, 7.617267],
+ 
+
+var map = L.map('map',{
+ center: [51.959312,7.617267],
  zoom: 12,
  minZoom: 11
  });
+ 
+ 
 
- L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
- attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
- }).addTo(map);
+ map.setMaxBounds([[51.8675880511,7.3663330078], [52.0554460954,7.8524780273]]);
+var osmLayer = new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { noWrap: true}).addTo(map);
+
+
 
  var sidebar = L.control.sidebar('sidebar').addTo(map);
