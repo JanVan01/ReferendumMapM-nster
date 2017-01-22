50dcad96aa055f49a55ddcd514bb8b4159997bd6
JanVan01 <j_vanz01@uni-muenster.de>
2017-01-22 20:09:32 +0100

    show alos invalid votes

--- js/data.js
+++ js/data.js
@@ -3,7 +3,7 @@ PREFIX foaf: <http://xmlns.com/foaf/0.1/>\
 PREFIX ref: <http://course.geoinfo2016.org/G1/vocabulary/ref#>\
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\
 SELECT DISTINCT\
-?name ?total ?yes ?no ?wkt \
+?name ?total ?yes ?no ?invalid ?wkt \
 WHERE {\
 GRAPH <http://course.geoinfo2016.org/G1>{\
 ?district rdf:type ref:District;\
@@ -11,6 +11,7 @@ foaf:name ?name;\
 ref:hasTotalVoters ?total;\
 ref:hasYesVotes ?yes;\
 ref:hasNoVotes ?no;\
+ref:hasInvalidVotes ?invalid;\
 geo:hasGeometry ?geo.\
 ?geo geo:hasSerialization ?wkt.\
 }}';
@@ -45,6 +46,7 @@ function sparql2GeoJSON(input) {
         feature.properties.totalVoters = parseInt(entry.total.value);
         feature.properties.yes = parseInt(entry.yes.value);
         feature.properties.no = parseInt(entry.no.value);
+        feature.properties.invalid = parseInt(entry.invalid.value);
         output.push(feature);
     }
     return output;
@@ -68,7 +70,7 @@ function getFillColor(feature){
 }
 
 function getOpacity(feature) {
-    return (feature.properties.no + feature.properties.yes)*3/feature.properties.totalVoters;
+    return (feature.properties.no + feature.properties.yes + feature.properties.invalid)*3/feature.properties.totalVoters;
 }
 
 function zoomToFeature(feature){
@@ -84,6 +86,7 @@ function onEachFeature(feature, layer){
         popupContent.push("<b><br/>Number of voters: </b>" + feature.properties.totalVoters)
         popupContent.push("<b><br/>Yes votes: </b>" + feature.properties.yes)
         popupContent.push("<b><br/>No votes: </b>" + feature.properties.no)
+        popupContent.push("<b><br/>Invalid votes: </b>" + feature.properties.invalid)
         layer.bindPopup("<p>" + popupContent.join("") + "</p>");
     }
 
